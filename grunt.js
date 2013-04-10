a74329df6374fbb7a5eb0ea046ab2c85b64e057f
Dustin J. Mitchell <dustin@mozilla.com>
2013-04-09 20:19:35 -0400

    add google webmaster tools proof of ownership

--- grunt.js
+++ grunt.js
@@ -172,6 +172,7 @@ module.exports = function (grunt) {
                     './temp/scripts/libs/': './src/scripts/libs/',
                     './temp/img/': './src/img/',
                     './temp/font/': './src/font/',
+                    './temp/google7cb1f653b4fd6f89.html': './src/google7cb1f653b4fd6f89.html',
                     './temp/robots.txt': './src/robots.txt'
                 }
             },
@@ -196,6 +197,7 @@ module.exports = function (grunt) {
                     './built/scripts/': './temp/scripts/scripts.min.js',
                     './built/scripts/libs': ['./temp/scripts/libs/html5shiv-printshiv.js', './temp/scripts/libs/json2.js'],
                     './built/styles/': './temp/styles/styles.min.css',
+                    './built/google7cb1f653b4fd6f89.html': './temp/google7cb1f653b4fd6f89.html',
                     './built/robots.txt': './temp/robots.txt',
                     './built/': './temp/min/*.html'
                 }
