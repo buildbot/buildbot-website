145726ae775979d5b0a5d4ddab437c12b9328949
Álvaro Osvaldo <alvaro@osvaldo.dev.br>
2024-08-22 09:55:09 -0300

    Fixed bug that prevented 'live reload' to run
    - Fixed string substitution on 'watch.sh'
    - Changed call for 'watch.sh' in 'package.json'
    - Change script interpreter to 'sh'

--- watch.sh
+++ watch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Kill parallel tasks together.
 # http://unix.stackexchange.com/a/107405
@@ -10,7 +10,7 @@ killgroup () {
   kill 0
 }
 
-dir=$(dirname "${BASH_SOURCE[0]}")
+dir="$(dirname "${0}")"
 
 $dir/node_modules/harp/bin/harp src &
 node $dir/node_modules/livereloadx/bin/livereloadx.js --include '**/*.{jade,less}' &
